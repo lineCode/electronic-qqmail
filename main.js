51b125905fbea265d3f7c44817f0157effae4968
zyt3000k <181958825@qq.com>
2017-10-16 16:57:07 +0800

    优化:邮件中链接打开方式

--- main.js
+++ main.js
@@ -1,50 +1,68 @@
 'use strict';
 
-const {app,shell, BrowserWindow} = require('electron')
+const {app, shell, BrowserWindow} = require('electron')
 const {Menu, Tray, nativeImage} = require('electron')
 const path = require('path')
-const url = require('url')
+const {URL, URLSearchParams} = require('url');
 const {ipcMain} = require('electron')
 
-let trayIcon = nativeImage.createFromPath(path.join(__dirname,`./image/Email_Chat.png`));
-let trayIconUnread = nativeImage.createFromPath(path.join(__dirname,'./image/Email_Chat_un.png'));
+let trayIcon =
+    nativeImage.createFromPath(path.join(__dirname, `./image/Email_Chat.png`));
+let trayIconUnread = nativeImage.createFromPath(
+    path.join(__dirname, './image/Email_Chat_un.png'));
 let win
 
-function createWindow () {
+function createWindow() {
   win = new BrowserWindow({
-    width: 1024,
-    height: 660,
-    icon:'./Email_Chat.png',
-    webPreferences:{
-      javascript: true,
-        plugins: true,
-        nodeIntegration: false,
-        webSecurity: false,
-      preload:path.join(__dirname, './preload.js'),
-      },
-    })
+    width : 1024,
+    height : 660,
+    icon : './Email_Chat.png',
+    webPreferences : {
+      javascript : true,
+      plugins : true,
+      nodeIntegration : false,
+      webSecurity : false,
+      preload : path.join(__dirname, './preload.js'),
+    },
+  })
   win.loadURL('https://exmail.qq.com/cgi-bin/loginpage')
   win.center()
   win.on('close', (e) => {
     // win.hide();
     if (win.isVisible()) {
-        e.preventDefault();
-        win.hide();
+      e.preventDefault();
+      win.hide();
+    }
+  })
+  win.webContents.on('new-window', (event, url) => {
+
+    console.log(url);
+    let myURL = new URL(url);
+    let hostname = myURL.hostname
+    if (hostname == 'exmail.qq.com') {
+      let pathname = myURL.pathname
+      if (pathname == '/cgi-bin/mail_spam') {
+        let action = myURL.searchParams.get('action')
+        let targetUrl = myURL.searchParams.get('url')
+        if (action == 'check_link') {
+          console.log(targetUrl);
+          event.preventDefault();
+          shell.openExternal(targetUrl);
+        }
       }
     }
-  )
-  win.webContents.on('new-window', (event, url) => {
+    else {
       event.preventDefault();
-      shell.openExternal(url);
-    });
-
-}
-function toggle(){
-    if (win.isVisible()) {
-      win.hide();
-    } else {
-      win.show();
+      shell.openExternal(targetUrl);
     }
+  });
+}
+function toggle() {
+  if (win.isVisible()) {
+    win.hide();
+  } else {
+    win.show();
+  }
 }
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -60,25 +78,23 @@ app.on('activate', () => {
 let appIcon = null
 app.on('ready', () => {
   createWindow()
-  appIcon = new Tray(trayIcon)
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'show',click:() => {win.show(); }},
-    {label: 'exit',click: () => { app.exit(0); }}
-  ])
+appIcon = new Tray(trayIcon)
+const contextMenu = Menu.buildFromTemplate([
+  {label : 'show', click : () => { win.show(); }},
+  {label : 'exit', click : () => { app.exit(0); }}
+])
   appIcon.setContextMenu(contextMenu)
 })
-let lastcount='unhas';
-ipcMain.on('has-un-count-tray',(event, arg) => {
-  //console.log(arg)  // prints "ping"
-  if(arg=='has'&&arg!=lastcount){
-    appIcon.setImage(trayIconUnread)
-    lastcount='has'
-  }
-  if(arg=='unhas'&& arg!=lastcount){
-    appIcon.setImage(trayIcon)
-    lastcount='unhas'
-  }
-})
-ipcMain.on('tips',(event,arg) => {
-  appIcon.setToolTip(arg)
-})
+  let lastcount = 'unhas';
+  ipcMain.on('has-un-count-tray', (event, arg) => {
+    // console.log(arg)  // prints "ping"
+    if (arg == 'has' && arg != lastcount) {
+      appIcon.setImage(trayIconUnread)
+      lastcount = 'has'
+    }
+    if (arg == 'unhas' && arg != lastcount) {
+      appIcon.setImage(trayIcon)
+      lastcount = 'unhas'
+    }
+  })
+  ipcMain.on('tips', (event, arg) => {appIcon.setToolTip(arg)})
