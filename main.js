0d53fe7d56118cd7a594622e91b9e0a7bdac1c27
zyt3000k <181958825@qq.com>
2017-10-20 11:20:30 +0800

    优化:程序退出时清理缓存

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const {app, shell, BrowserWindow} = require('electron')
-const {Menu, Tray, nativeImage} = require('electron')
+const { app, shell, BrowserWindow } = require('electron')
+const { Menu, Tray, nativeImage } = require('electron')
 const path = require('path')
-const {URL, URLSearchParams} = require('url');
-const {ipcMain} = require('electron')
+const { URL, URLSearchParams } = require('url');
+const { ipcMain } = require('electron')
+const mailNotify = require('./notify/notify.js')
 
 let trayIcon =
     nativeImage.createFromPath(path.join(__dirname, `./image/Email_Chat.png`));
@@ -13,92 +14,103 @@ let trayIconUnread = nativeImage.createFromPath(
 let win
 
 function createWindow() {
-  win = new BrowserWindow({
-    width : 1024,
-    height : 660,
-    icon : './Email_Chat.png',
-    webPreferences : {
-      javascript : true,
-      plugins : true,
-      nodeIntegration : false,
-      webSecurity : false,
-      preload : path.join(__dirname, './preload.js'),
-    },
-  })
-  win.loadURL('https://exmail.qq.com/cgi-bin/loginpage')
-  win.center()
-  win.on('close', (e) => {
-    // win.hide();
-    if (win.isVisible()) {
-      e.preventDefault();
-      win.hide();
-    }
-  })
-  win.webContents.on('new-window', (event, url) => {
-
-    console.log(url);
-    let myURL = new URL(url);
-    let hostname = myURL.hostname
-    if (hostname == 'exmail.qq.com') {
-      let pathname = myURL.pathname
-      if (pathname == '/cgi-bin/mail_spam') {
-        let action = myURL.searchParams.get('action')
-        let targetUrl = myURL.searchParams.get('url')
-        if (action == 'check_link') {
-          console.log(targetUrl);
-          event.preventDefault();
-          shell.openExternal(targetUrl);
-          return;
+    win = new BrowserWindow({
+        width: 1024,
+        height: 660,
+        icon: './Email_Chat.png',
+        show: false,
+        webPreferences: {
+            javascript: true,
+            plugins: true,
+            nodeIntegration: false,
+            webSecurity: false,
+            preload: path.join(__dirname, './preload.js'),
+        },
+    })
+    win.loadURL('https://exmail.qq.com/cgi-bin/loginpage')
+    win.center()
+    win.on('close', (e) => {
+        // win.hide();
+        if (win.isVisible()) {
+            e.preventDefault();
+            win.hide();
         }
-      }
-      if (pathname == '/cgi-bin/readmail' || pathname == '/cgi-bin/download') {
-        return;
-      }
-    }
+    })
+    win.once('ready-to-show', () => { win.show() })
+    win.webContents.on('new-window', (event, url) => {
 
-    event.preventDefault();
-    shell.openExternal(url);
+        console.log(url);
+        let myURL = new URL(url);
+        let hostname = myURL.hostname
+        if (hostname == 'exmail.qq.com') {
+            let pathname = myURL.pathname
+            if (pathname == '/cgi-bin/mail_spam') {
+                let action = myURL.searchParams.get('action')
+                let targetUrl = myURL.searchParams.get('url')
+                if (action == 'check_link') {
+                    console.log(targetUrl);
+                    event.preventDefault();
+                    shell.openExternal(targetUrl);
+                    return;
+                }
+            }
+            if (pathname == '/cgi-bin/readmail' || pathname == '/cgi-bin/download') {
+                return;
+            }
+        }
 
-  });
+        event.preventDefault();
+        shell.openExternal(url);
+
+    });
 }
+
 function toggle() {
-  if (win.isVisible()) {
-    win.hide();
-  } else {
-    win.show();
-  }
+    if (win.isVisible()) {
+        win.hide();
+    } else {
+        win.show();
+    }
 }
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+    if (win === null) {
+        createWindow()
+    }
 })
 let appIcon = null
 app.on('ready', () => {
-  createWindow()
-appIcon = new Tray(trayIcon)
-const contextMenu = Menu.buildFromTemplate([
-  {label : 'show', click : () => { win.show(); }},
-  {label : 'exit', click : () => { app.exit(0); }}
-])
-  appIcon.setContextMenu(contextMenu)
+    createWindow()
+    appIcon = new Tray(trayIcon)
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'show', click: () => { win.show(); } },
+        {
+            label: 'exit',
+            click: () => {
+                win.webContents.session.clearCache(function() { console.log('clearCache') });
+                win.webContents.session.clearStorageData(function() { console.log('clearStorageData'); });
+                app.exit(0);
+            }
+        },
+        // { label: 'notify', click: () => { new mailNotify().show() } },
+    ])
+    appIcon.setContextMenu(contextMenu)
 })
-  let lastcount = 'unhas';
-  ipcMain.on('has-un-count-tray', (event, arg) => {
+let lastcount = 'unhas';
+ipcMain.on('has-un-count-tray', (event, arg) => {
     // console.log(arg)  // prints "ping"
     if (arg == 'has' && arg != lastcount) {
-      appIcon.setImage(trayIconUnread)
-      lastcount = 'has'
+        appIcon.setImage(trayIconUnread)
+        lastcount = 'has'
     }
     if (arg == 'unhas' && arg != lastcount) {
-      appIcon.setImage(trayIcon)
-      lastcount = 'unhas'
+        appIcon.setImage(trayIcon)
+        lastcount = 'unhas'
     }
-  })
-  ipcMain.on('tips', (event, arg) => {appIcon.setToolTip(arg)})
+})
+ipcMain.on('tips', (event, arg) => { appIcon.setToolTip(arg) })
